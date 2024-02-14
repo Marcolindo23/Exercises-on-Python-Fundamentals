84f626ed155d8694092e5a80a67e428dca93c320
Marcolindo23 <marco.giusepponi1@gmail.com>
2024-02-14 18:32:48 +0100

    esercizi aggiornati

--- primo.py
+++ primo.py
@@ -19,3 +19,50 @@ r = 6371
 λ2 = radians(-123.1)
 d = 2*r*asin(sqrt(sin(1/2*(ϕ2-ϕ1))**2+cos(ϕ1)*cos(ϕ2)*sin(1/2*(λ2-λ1))**2))
 print(d)
+
+"""Definire
+- due variabili (i1, i2) di tipo intero
+- due variabili (s1, s2) di tipo stringa
+- due variabili (f1, f2) di tipo float
+- due variabili (b1, b2) di tipo boolean
+stampare
+i1+i2
+s1+s2
+f1+f2
+b1+b2
+i1+s1
+s1+i2
+i1+f2
+f2+i2
+i1+b1
+f1+b1
+s1+b1
+b1+i1
+b1+s1
+b1+f1"""
+
+i1=70
+i2=90
+s1="buonasera"
+s2="buongiorno"
+f1=127.9
+f2=135.7
+b1=True
+b2=False
+
+print(i1+i2)
+print(s1+s2)
+print(f1+f2)
+print(b1+b2)
+#print(i1+s1)
+#print(s1+i2)
+print(i1+f2)
+print(f2+i2)
+print(i1+b1)
+print(f1+b1)
+#print(s1+b1)
+print(b1+i1)
+#print(b1+s1)
+print(b1+f1)
+
+
